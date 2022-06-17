f054c9c29af3987d790775af1b607b60b793bf3c
Michael Dillahunty <michaeldillahunty01@gmail.com>
2022-06-16 19:20:34 -0500

    unimportant

--- commands.js
+++ commands.js
@@ -54,6 +54,6 @@ banner = [
   "╚═════╝░╚═╝╚══════╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░╚═════╝░╚═╝░░╚══╝░░░╚═╝░░░░░░╚═╝░░░",
 // "█▀▄▀█ █ █▀▀ █░█ ▄▀█ █▀▀ █░░   █▀▄ █ █░░ █░░ ▄▀█ █░█ █░█ █▄░█ ▀█▀ █▄█",
 // "█░▀░█ █ █▄▄ █▀█ █▀█ ██▄ █▄▄   █▄▀ █ █▄▄ █▄▄ █▀█ █▀█ █▄█ █░▀█ ░█░ ░█░",
-  '<span class="color2">Welcome to my interactive portfolio.</span>',
+  '<span class="color2">Welcome to my interactive portfolio!</span>',
   "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
 ];
