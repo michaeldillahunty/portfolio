379ba2966591dab8f0f22dcbd971d3b7c96359b7
Michael Dillahunty <michaeldillahunty01@gmail.com>
2023-06-12 11:50:06 -0500

    Update portfolio photos

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
    {
       title: 'Four-in-a-Row AI Solver',
-      imgUrl: 'public/assets/4-in-a-row.jpg',
+      imgUrl: '/assets/4-in-a-row.jpg',
       stack: ['Python', 'AI', 'MRV', 'Minimax'], // i.e. 'HTML', 'CSS', 'JavaScript'
       link: 'https://github.com/michaeldillahunty/CS4750-hw4/tree/main'
    },
    {
       title: 'Custom Python Parser',
-      imgUrl: 'public/assets/parser.jpg',
+      imgUrl: '/assets/parser.jpg',
       stack: ['Java', 'ANTLR', 'CFG'], // i.e. 'HTML', 'CSS', 'JavaScript'
       link: 'https://github.com/michaeldillahunty/CS4450-Parser-Final-Project'
    },
    {
       title: 'In progress...',
-      imgUrl: 'public/assets/Screenshot 2023-06-06 at 7.02.53 PM.jpg',
+      imgUrl: '/assets/cff_logo.jpg',
       stack: ['React', 'Java', 'MongoDB', 'AWS'], // i.e. 'HTML', 'CSS', 'JavaScript'
       link: 'https://somelink.com'
    },
